2d75107e2e85c0845bd08031371df1c90691d658
mooncrown04 <mooncrown04@gmail.com>
2026-03-08 21:05:25 +0300

    Update netmirror.js

--- providers/netmirror.js
+++ providers/netmirror.js
@@ -111,7 +111,7 @@ function searchContent(query, platform) {
     return bypass().then(function (cookie) {
         const cookies = {
             't_hash_t': cookie,
-            'user_token': '233123f803cf02184bf6c67e149cdd50',
+            'user_token': 'd753a3a2f2aa85e0abb7e334574ffc31%3A%3A0c69f152f07d0f5e9f7555b314c88029%3A%3A1772982973%3A%3Art',            
             'hd': 'on',
             'ott': ott
         };
