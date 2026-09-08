990ff1d3a56c7f2e32f33c6633a72924dfbd720d
Falsis <keremtokul@hotmail.com>
2026-09-09 00:20:23 +0300

    fix: redirect vercel traffic to nuviotr.netlify.app and use static CGO_ENABLED=0 build for Netlify

--- cmd/netlify/main.go
+++ cmd/netlify/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"log"
 	"net/http"
+	"runtime/debug"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -13,10 +16,33 @@ import (
 var adapter *httpadapter.HandlerAdapter
 
 func init() {
+	defer func() {
+		if r := recover(); r != nil {
+			log.Printf("FATAL INIT PANIC: %v\nStack: %s", r, string(debug.Stack()))
+		}
+	}()
 	adapter = httpadapter.New(http.HandlerFunc(handler.Handler))
 }
 
-func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (resp events.APIGatewayProxyResponse, err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			log.Printf("PANIC in Handler: %v\nStack: %s", r, string(debug.Stack()))
+			resp = events.APIGatewayProxyResponse{
+				StatusCode: 500,
+				Headers: map[string]string{
+					"Content-Type": "application/json",
+				},
+				Body: fmt.Sprintf(`{"error":"internal_server_error","detail":"%v"}`, r),
+			}
+			err = nil
+		}
+	}()
+
+	if adapter == nil {
+		adapter = httpadapter.New(http.HandlerFunc(handler.Handler))
+	}
+
 	return adapter.ProxyWithContext(ctx, req)
 }
 
